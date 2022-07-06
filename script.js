c7befa3785777dc8c37e3d86fe3160bda68109e6
robertosolari <66957110+robertosolari@users.noreply.github.com>
2022-07-07 01:03:34 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 async function fetchData() {
-    const res=await fetch ("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-latest.csv");
-    const record=await res.json();
-    document.getElementById("data").innerHTML=record.data[0].date;
-    document.getElementById("stato").innerHTML=record.data[0].areaName;
-    document.getElementById("totale_positivi").innerHTML=record.data[0].latestBy;
-    document.getElementById("deceduti").innerHTML=record.data[0].deathNew;
+    const res = await fetch ("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json");
+    const record = await res.json();
+    
+    document.getElementById("data").innerHTML               =   record[0].data;
+    document.getElementById("stato").innerHTML              =   record[0].stato;
+    document.getElementById("totale_positivi").innerHTML    =   record[0].totale_positivi;
+    document.getElementById("deceduti").innerHTML           =   record[0].deceduti;
 }
 fetchData();
